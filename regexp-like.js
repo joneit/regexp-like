6f7b7d5af61cd017e849010f111f0f750d6777d6
Jonathan Eiten <jonathan@openfin.co>
2016-01-06 10:54:45 -0500

    (See gh-pages.sh on master branch.)

---
+++ regexp-like.js
@@ -0,0 +1,164 @@
+'use strict';
+
+var // a regex search pattern that matches all the reserved chars of a regex search pattern
+    reserved = /([\.\\\+\*\?\^\$\(\)\{\}\=\!\<\>\|\:\[\]])/g,
+
+    // regex wildcard search patterns
+    REGEXP_WILDCARD = '.*',
+    REGEXP_WILDCHAR = '.',
+    REGEXP_WILDCARD_MATCHER = '(' + REGEXP_WILDCARD + ')',
+
+    // LIKE search patterns
+    LIKE_WILDCHAR = '_',
+    LIKE_WILDCARD = '%',
+
+    // regex search patterns that match LIKE search patterns
+    REGEXP_LIKE_PATTERN_MATCHER = new RegExp('(' + [
+        LIKE_WILDCHAR,
+        LIKE_WILDCARD,
+        '\\[\\^?[^-\\]]+]', // matches a LIKE set (same syntax as a RegExp set)
+        '\\[\\^?[^-\\]]\\-[^\\]]]' // matches a LIKE range (same syntax as a RegExp range)
+    ].join('|') + ')', 'g');
+
+function regExpLIKE(pattern, ignoreCase) {
+    var i, parts;
+
+    // Find all LIKE patterns
+    parts = pattern.match(REGEXP_LIKE_PATTERN_MATCHER);
+
+    if (parts) {
+        // Translate found LIKE patterns to regex patterns, escaped intervening non-patterns, and interleave the two
+
+        for (i = 0; i < parts.length; ++i) {
+            // Escape left brackets (unpaired right brackets are OK)
+            if (parts[i][0] === '[') {
+                parts[i] = regExpLIKE.reserve(parts[i]);
+            }
+
+            // Make each found pattern matchable by enclosing in parentheses
+            parts[i] = '(' + parts[i] + ')';
+        }
+
+        // Match these precise patterns again with their intervening non-patterns (i.e., text)
+        parts = pattern.match(new RegExp(
+            REGEXP_WILDCARD_MATCHER +
+            parts.join(REGEXP_WILDCARD_MATCHER)  +
+            REGEXP_WILDCARD_MATCHER
+        ));
+
+        // Discard first match of non-global search (which is the whole string)
+        parts.shift();
+
+        // For each re-found pattern part, translate % and _ to regex equivalent
+        for (i = 1; i < parts.length; i += 2) {
+            var part = parts[i];
+            switch (part) {
+                case LIKE_WILDCARD: part = REGEXP_WILDCARD; break;
+                case LIKE_WILDCHAR: part = REGEXP_WILDCHAR; break;
+                default:
+                    var j = part[1] === '^' ? 2 : 1;
+                    part = '[' + regExpLIKE.reserve(part.substr(j, part.length - (j + 1))) + ']';
+            }
+            parts[i] = part;
+        }
+    } else {
+        parts = [pattern];
+    }
+
+    // For each surrounding text part, escape reserved regex chars
+    for (i = 0; i < parts.length; i += 2) {
+        parts[i] = regExpLIKE.reserve(parts[i]);
+    }
+
+    // Join all the interleaved parts
+    parts = parts.join('');
+
+    // Optimize or anchor the pattern at each end as needed
+    if (parts.substr(0, 2) === REGEXP_WILDCARD) { parts = parts.substr(2); } else { parts = '^' + parts; }
+    if (parts.substr(-2, 2) === REGEXP_WILDCARD) { parts = parts.substr(0, parts.length - 2); } else { parts += '$'; }
+
+    // Return the new regex
+    return new RegExp(parts, ignoreCase ? 'i' : undefined);
+}
+
+regExpLIKE.reserve = function (s) {
+    return s.replace(reserved, '\\$1');
+};
+
+var cache, size;
+
+/**
+ * @summary Delete a pattern from the cache; or clear the whole cache.
+ * @param {string} [pattern] - The LIKE pattern to remove from the cache. Fails silently if not found in the cache. If pattern omitted, clears whole cache.
+ */
+(regExpLIKE.clearCache = function (pattern) {
+    if (!pattern) {
+        cache = {};
+        size = 0;
+    } else if (cache[pattern]) {
+        delete cache[pattern];
+        size--;
+    }
+    return size;
+})(); // init the cache
+
+regExpLIKE.getCacheSize = function () { return size; };
+
+/**
+ * @summary Cached version of `regExpLIKE()`.
+ * @desc Cached entries are subject to garbage collection if `keep` is `undefined` or `false` on insertion or `false` on most recent reference. Garbage collection will occur iff `regExpLIKE.cacheMax` is defined and it equals the number of cached patterns. The garbage collector sorts the patterns based on most recent reference; the oldest 10% of the entries are deleted. Alternatively, you can manage the cache yourself to a limited extent (see {@link regeExpLIKE.clearCache|clearCache}).
+ * @param pattern - the LIKE pattern (to be) converted to a RegExp
+ * @param [keep] - If given, changes the keep status for this pattern as follows:
+ * * `true` permanently caches the pattern (not subject to garbage collection) until `false` is given on a subsequent call
+ * * `false` allows garbage collection on the cached pattern
+ * * `undefined` no change to keep status
+ * @returns {RegExp}
+ */
+regExpLIKE.cached = function (keep, pattern, ignoreCase) {
+    if (typeof keep === 'string') {
+        ignoreCase = pattern;
+        pattern = keep;
+        keep = false;
+    }
+    var patternAndCase = pattern + (ignoreCase ? 'i' : 'c'),
+        item = cache[patternAndCase];
+    if (item) {
+        item.when = new Date().getTime();
+        if (keep !== undefined) {
+            item.keep = keep;
+        }
+    } else {
+        if (size === regExpLIKE.cacheMax) {
+            var age = [], ages = 0, key, i;
+            for (key in cache) {
+                item = cache[key];
+                if (!item.keep) {
+                    for (i = 0; i < ages; ++i) {
+                        if (item.when < age[i].item.when) {
+                            break;
+                        }
+                    }
+                    age.splice(i, 0, { key: key, item: item });
+                    ages++;
+                }
+            }
+            if (!age.length) {
+                return regExpLIKE(pattern, ignoreCase); // cache is full!
+            }
+            i = Math.ceil(age.length / 10); // will always be at least 1
+            size -= i;
+            while (i--) {
+                delete cache[age[i].key];
+            }
+        }
+        item = cache[patternAndCase] = {
+            regex: regExpLIKE(pattern, ignoreCase),
+            keep: keep,
+            when: new Date().getTime()
+        };
+        size++;
+    }
+    return item.regex;
+};
+
+window.regExpLIKE = regExpLIKE;
